48c02c4a714ee1e03fd94de4140922ec63d2b958
WilhelmK109 <kamulungaw@gmail.com>
2023-06-06 11:14:52 +0200

    Add other project file dependencies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import TodoApp from './components/TodoApp';
 
 function App() {
   return (
     <div className="App">
-      
+      <TodoApp />
     </div>
   );
 }
